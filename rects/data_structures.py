0d3ae86aa902fadcad5a3afeefe8b07b4ead527b
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-02 19:47:14 -0500

    Initial commit.

---
+++ rects/data_structures.py
@@ -0,0 +1,94 @@
+from typing import NamedTuple
+
+
+class Size(NamedTuple):
+    height: int
+    width: int
+
+
+class Point(NamedTuple):
+    y: int
+    x: int
+
+    def __add__(self, other: Size):
+        if not isinstance(other, Size):
+            return NotImplemented
+
+        y, x = self
+        h, w = other
+
+        return Point(y + h, x + w)
+
+
+class Rect(NamedTuple):
+    """
+    A rect is a rectangular region specified with a point and a size.
+    """
+    topleft: Point
+    size: Size
+
+    @property
+    def height(self):
+        return self.size.height
+
+    @property
+    def width(self):
+        return self.size.width
+
+    @property
+    def topright(self):
+        return Point(
+            self.topleft.y,
+            self.topleft.x + self.width,
+        )
+
+    @property
+    def bottomleft(self):
+        return Point(
+            self.topleft.y + self.height,
+            self.topleft.x,
+        )
+
+    @property
+    def bottomright(self):
+        return self.topleft + self.size
+
+    def __contains__(self, point: Point):
+        py, px = point
+
+        y1, x1 = self.topleft
+        y2, x2 = self.bottomright
+
+        return y1 <= py <= y2 and x1 <= px <= x2
+
+    # TODO: intersects
+
+
+class Band(NamedTuple):
+    """
+    A band is a horizontal region (with a top/bottom) and a list of walls.
+    """
+    top: int
+    bottom: int
+    walls: list[int]
+
+    @property
+    def rects(self):
+        """
+        Yield the Rects that make up the band.
+        """
+        top = self.top
+        height = self.bottom - top
+
+        it = iter(self.walls)
+        for x1, x2 in zip(it, it):
+            yield Rect(
+                Point(top, x1),
+                Size(height, x2 - x1),
+            )
+
+    def __len__(self):
+        """
+        Number of rects in the band.
+        """
+        return len(self.walls) // 2
