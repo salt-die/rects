a57be58127ec9b024bd460fed61f214808e10f06
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-05 19:02:09 -0500

    Threshold is emitted too.

--- rects/merge.py
+++ rects/merge.py
@@ -1,6 +1,6 @@
 def merge(a, b):
     """
-    Emit in/out signals whenever a threshold is cross.
+    Emit in/out signals whenever a threshold is crossed.
     """
     a = iter(a)
     b = iter(b)
@@ -12,14 +12,30 @@ def merge(a, b):
     in_b = 0
 
     while current_a is not None and current_b is not None:
-        old_a, old_b = current_a, current_b
+        threshold = min(current_a, current_b)
 
-        if old_a >= old_b:
-            in_b ^= 1
-            current_b = next(b, None)
-
-        if old_b >= old_a:
+        if current_a == threshold:
             in_a ^= 1
             current_a = next(a, None)
 
-        yield in_a, in_b
+        if current_b == threshold:
+            in_b ^= 1
+            current_b = next(b, None)
+
+        yield in_a, in_b, threshold
+
+    if current_a is not None:
+        in_a ^= 1
+        yield in_a, in_b, current_a
+
+        for current_a in a:
+            in_a ^= 1
+            yield in_a, in_b, current_a
+
+    elif current_b is not None:
+        in_b ^= 1
+        yield in_a, in_b, current_b
+
+        for current_b in b:
+            in_b ^= 1
+            yield in_a, in_b, current_b
