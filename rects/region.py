3c2045a3e2a112767afe891414c126140cbbbee6
salt-die <53280662+salt-die@users.noreply.github.com>
2021-10-18 14:45:09 -0500

    `repr` and `_reband` implemented.

--- rects/region.py
+++ rects/region.py
@@ -1,13 +1,17 @@
-from .band import Band, INF_BAND
-from .data_structures import Rect
+from math import inf
+
+from .band import Band
+from .data_structures import Rect, Interval
 
 
 class Region:
     """
     Collection of mutually exclusive bands.
     """
+    __slots__ = 'bands',
+
     def __init__(self):
-        self.bands = [ INF_BAND ]
+        self.bands: list[Band] = [ Band(Interval(-inf, inf), [-inf, inf]) ]
 
     def __and__(self, other: Rect):
         raise NotImplementedError()
@@ -37,3 +41,37 @@ class Region:
         """
         Join contiguous bands that have identical walls.
         """
+
+    def _reband(self, rect: Rect):
+        """
+        Adjust band topbottom intervals to accomodate rect.
+        """
+        bands = self.bands
+        rect_bands = [ rect_band ] = [ Band(rect.topbottom, [*rect.leftright]) ]
+        top, bottom = rect_band.topbottom
+
+        i = 0
+        while i < len(bands):
+            band = bands[i]
+
+            if top != band.top and top in band.topbottom:
+                bands.insert(i + 1, band.split(top))
+            elif bottom != band.top and bottom in band.topbottom:
+                bands.insert(i + 1, band.split(bottom))
+            elif band.top != top and band.top in rect_band.topbottom:
+                rect_bands[-1:] = [rect_band] = [rect_band.split(band.top)]
+                top, bottom = rect_band.topbottom
+            elif band.bottom != top and band.bottom in rect_band.topbottom:
+                rect_bands[-1:] = [rect_band] = [rect_band.split(band.bottom)]
+                top, bottom = rect_band.topbottom
+
+            i += 1
+
+        return rect_bands
+
+    def __repr__(self):
+        attrs = ', '.join(
+            f'{attr}={getattr(self, attr)}'
+            for attr in self.__slots__
+        )
+        return f'{type(self).__name__}({attrs})'
